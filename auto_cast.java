f55f9f0b8efba2eb811ef4a1946cd9c2ecc5ddcd
HARSHA91822 <ashwithpatelramannagari@gmail.com>
2025-08-05 11:21:23 +0530

    auto cast

---
+++ auto_cast.java
@@ -0,0 +1,14 @@
+package hmproject;
+
+public class auto_cast {
+	public static void main(String[]args) {
+		int myInt=9;
+		double myDouble=myInt;
+		//automatic casting:int  to double
+		System.out.println(myInt);
+		System.out.println(myDouble);
+		System.out.println(myDouble);
+		System.out.println(myInt);
+	}
+
+}
